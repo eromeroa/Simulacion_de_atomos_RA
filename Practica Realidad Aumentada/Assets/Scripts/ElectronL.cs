8b178289afd9d42c6e79f103eadbfe18c765b65f
eromeroa <e.romeroa@alumnos.upm.es>
2019-05-09 20:59:01 +0200

    añadidos scripts para los electrones de cada capa

---
+++ Practica Realidad Aumentada/Assets/Scripts/ElectronL.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElectronL : MonoBehaviour
+{
+    Renderer rend;
+    public GameObject padre;
+    public float velocidad;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        velocidad = 35.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.RotateAround(padre.transform.position, padre.transform.up, velocidad * Time.deltaTime);
+    }
+}
